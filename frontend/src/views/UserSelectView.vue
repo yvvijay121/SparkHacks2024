63b968de296659801e3f16db73265d68f043e34a
twonkista <65195116+twonkista@users.noreply.github.com>
2024-02-10 10:21:40 -0600

    added fetch requests

--- frontend/src/views/UserSelectView.vue
+++ frontend/src/views/UserSelectView.vue
@@ -1,9 +1,29 @@
-<script setup>
-
+script>
+export default {
+    name: "get-request",
+    data() {
+        return {
+            users: [],
+        };
+    },
+    created(){
+        fetch("http://127.0.0.1:105/get_users")
+        .then(response => response.json())
+        .then(data => (this.users.users[0] = data))
+    }
+};
 </script>
 
 <template>
   <div class="box">
     <h1>Select User</h1>
+  </div>
+  <div class="box">
+    <label class = "radio">
+        <input type = "radio">
+        Toggle Data |
+    </label>
+    <br>
+    {{ users.first_name }}
   </div>
 </template>
