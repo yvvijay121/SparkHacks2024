3a26a0a26391f9ae8141ff7e145d3f57035a7808
twonkista <65195116+twonkista@users.noreply.github.com>
2024-02-10 11:45:53 -0600

    worky

--- frontend/src/views/UserSelectView.vue
+++ frontend/src/views/UserSelectView.vue
@@ -1,15 +1,17 @@
+Vue.prototype.$hostname = 'http'
+
 <script>
 export default {
     name: "get-request",
     data() {
         return {
-            users: [],
+            users: {},
         };
     },
     created(){
         fetch("http://127.0.0.1:105/get_users")
         .then(response => response.json())
-        .then(data => (this.users.users[0] = data))
+        .then(data => (this.users = data.users));
     }
 };
 </script>
@@ -18,12 +20,24 @@ export default {
   <div class="box">
     <h1>Select User</h1>
   </div>
-  <div class="box">
-    <label class = "radio">
-        <input type = "radio">
-        Toggle Data |
-    </label>
-    <br>
-    {{ users.first_name }}
-  </div>
+  <div class = "li">
+    <li v-for="( users , index) in users">
+        <div class="box">
+            <label class = "radio">
+                <input type = "radio">
+                Show
+                <input type = "radio">
+                Hide | 
+                Toggle Data |
+            </label>
+                {{ users.first_name }} {{ users.last_name }}
+        </div>
+    </li>
+ </div>
 </template>
+
+<style>
+li{
+    list-style-type: none;
+}
+</style>
